5c3e29284b83be53174884e17af0c782bcb26791
qzh <2806894439@qq.com>
2023-09-13 22:40:47 +0800

    再来一单功能开发

--- sky-server/src/main/java/com/sky/controller/user/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -94,4 +94,17 @@ public class OrderController {
         orderService.userCancelById(id);
         return Result.success();
     }
+
+    /**
+     * 再来一单
+     * @param id
+     * @return
+     */
+    @PostMapping("/repetition/{id}")
+    @ApiOperation("再来一单")
+    public Result repetition(@PathVariable("id") Long id){
+        log.info("再来一单：{}",id);
+        orderService.repetition(id);
+        return Result.success();
+    }
 }
