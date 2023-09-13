c21643a23aaa6d8b48b869507b39a3f0474b815f
qzh <2806894439@qq.com>
2023-09-13 21:43:55 +0800

    查询订单详情功能开发

--- sky-server/src/main/java/com/sky/controller/user/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -7,6 +7,7 @@ import com.sky.result.Result;
 import com.sky.service.OrderService;
 import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderSubmitVO;
+import com.sky.vo.OrderVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -66,5 +67,18 @@ public class OrderController {
         log.info("历史订单查询");
         PageResult pageResult = orderService.pageQuery4User(page,pageSize,status);
         return Result.success(pageResult);
+    }
+
+    /**
+     * 查询订单详情
+     * @param id
+     * @return
+     */
+    @GetMapping("/orderDetail/{id}")
+    @ApiOperation("查询订单详情")
+    public Result<OrderVO> details(@PathVariable("id") Long id){
+        log.info("查询订单详情：{}",id);
+        OrderVO orderVO = orderService.details(id);
+        return Result.success(orderVO);
     }
 }
